ed8a6ef3c47d1e36dc58f1a77e49e1eb2829573d
mohamed ismail <ismail@pountech.com>
2020-01-16 11:32:44 +0200

    add docker

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
 FROM ubuntu:16.04
 
-RUN apt-get update -y && \
-    apt-get install -y python-pip python-dev
+RUN apt-get update -y
 
 # We copy just the requirements.txt first to leverage Docker cache
 COPY ./requirement.txt /app/requirements.txt
 
 WORKDIR /app
 
-RUN pip install -r requirements.txt
-
 COPY . /app
 
-ENTRYPOINT [ "python" ]
+RUN bash curlLinks.sh
 
-CMD [ "curl.py" ]
+EXPOSE 5000
+
+CMD ["bash","run.sh"]
--- curlLinks.sh
+++ curlLinks.sh
@@ -1,15 +1,21 @@
 #bash
 
-apt install libcurl4-openssl-dev libssl-dev
+apt install -y python3
 
-pip install --upgrade pip
+apt install -y python3-pip
 
-pip install pycurl
+apt install -y libcurl4-openssl-dev libssl-dev
 
-pip install Flask
+pip3 install --upgrade pip3
 
-pip install -r requirement.txt
+pip3 install pycurl
 
+pip3 install Flask
+
+pip3 install -r requirement.txt
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
 export FLASK_APP=curl.py
 
 
